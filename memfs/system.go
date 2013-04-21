4d212cca9ab84b5c387ab15f9e485b89ff105bc5
Naitik Shah <n@daaku.org>
2013-04-21 13:36:05 -0700

    memfs.NewWithFiles to automatically create directories

--- memfs/system.go
+++ memfs/system.go
@@ -1,11 +1,16 @@
 package memfs
 
 import (
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/daaku/go.fs"
+	"github.com/daaku/go.fs/emptyfs"
 	"github.com/daaku/go.fs/fsutil"
 )
 
-type system map[string]fs.File
+type system map[string]*File
 
 func (s system) Open(name string) (fs.File, error) {
 	if f := s[name]; f != nil {
@@ -14,12 +19,45 @@ func (s system) Open(name string) (fs.File, error) {
 	return nil, fsutil.NewErrNotFound(name)
 }
 
+func (s system) add(fullpath string, file *File) error {
+	fi, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	s[fullpath] = file
+	parent := filepath.Dir(fullpath)
+	if parentdir := s[parent]; parentdir != nil {
+		if err := parentdir.AddDirInfo(fi); err != nil {
+			return err
+		}
+	} else {
+		parentdir := NewDir(
+			filepath.Base(parent), os.FileMode(755), time.Now(), []os.FileInfo{fi})
+		if err := s.add(parent, parentdir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (s system) IsNotExist(err error) bool {
 	return fsutil.IsNotExist(err)
 }
 
 // Creates a fs.System backed by the given map. It expects directories to also
 // have provided entries as necessary and won't create them.
-func NewSystem(files map[string]fs.File) fs.System {
+func NewSystem(files map[string]*File) fs.System {
 	return system(files)
+}
+
+// Creates a fs.System backed by the given map. It expects only Files and will
+// generate Directory entries automatically.
+func NewWithFiles(files map[string]*File) fs.System {
+	s := system(make(map[string]*File))
+	for fullpath, file := range files {
+		if err := s.add(fullpath, file); err != nil {
+			return emptyfs.NewWithError(err)
+		}
+	}
+	return s
 }
--- memfs/system_test.go
+++ memfs/system_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/daaku/go.fs"
 	"github.com/daaku/go.fs/memfs"
 )
 
@@ -14,7 +13,7 @@ func TestSimpleSystem(t *testing.T) {
 	const name = "foo"
 	const data = "bar"
 	createdFile := memfs.NewFile(name, os.FileMode(666), time.Now(), []byte(data))
-	s := memfs.NewSystem(map[string]fs.File{name: createdFile})
+	s := memfs.NewSystem(map[string]*memfs.File{name: createdFile})
 	openedFile, err := s.Open(name)
 	if err != nil {
 		t.Fatal(err)
@@ -33,5 +32,60 @@ func TestSystemNotFound(t *testing.T) {
 	}
 	if !s.IsNotExist(err) {
 		t.Fatal("was expecting is not exist error")
+	}
+}
+
+func TestSystemWithFiles(t *testing.T) {
+	t.Parallel()
+	f1 := memfs.NewFile("foo", os.FileMode(666), time.Now(), nil)
+	f2 := memfs.NewFile("bar", os.FileMode(666), time.Now(), nil)
+	s := memfs.NewWithFiles(map[string]*memfs.File{
+		"d/foo": f1,
+		"d/bar": f2,
+	})
+	a1, err := s.Open("d/foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if a1 != f1 {
+		t.Fatal("did not find expected file")
+	}
+	d, err := s.Open("d")
+	if err != nil {
+		t.Fatal(err)
+	}
+	some, err := d.Readdirnames(0)
+	if len(some) != 2 {
+		t.Fatal("was expecting 2 names")
+	}
+	if actual := some[1]; actual != "bar" {
+		t.Fatal("was expecting bar")
+	}
+}
+
+func TestSystemWithFilesClosedFile(t *testing.T) {
+	t.Parallel()
+	f1 := memfs.NewFile("foo", os.FileMode(666), time.Now(), nil)
+	f1.Close()
+	s := memfs.NewWithFiles(map[string]*memfs.File{
+		"d/foo": f1,
+	})
+	_, err := s.Open("d/foo")
+	if err == nil {
+		t.Fatal("was expecting error")
+	}
+}
+
+func TestSystemWithFilesIncorrectStructure(t *testing.T) {
+	t.Parallel()
+	f1 := memfs.NewFile("foo", os.FileMode(666), time.Now(), nil)
+	f2 := memfs.NewFile("bar", os.FileMode(666), time.Now(), nil)
+	s := memfs.NewWithFiles(map[string]*memfs.File{
+		"d":     f2,
+		"d/foo": f1,
+	})
+	_, err := s.Open("d/foo")
+	if err == nil {
+		t.Fatal("was expecting error")
 	}
 }
